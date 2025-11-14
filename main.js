e7c154a8defa642877251d1ee50fc6c974042269
Daria bc2025-6 <kozikovadasa@gmail.com>
2025-11-14 21:12:24 +0200

    deleted html file and add new main.js

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
-const { program } = require('commander');
-const http = require('http');
+const express = require('express');
 const fs = require('fs');
+const http = require('http');
+const { program } = require('commander');
+const multer = require('multer');
 
 program
-  .option('-h, --host <host>')
-  .option('-p, --port <port>')
-  .option('-c, --cache <cache>');
+  .requiredOption('-h, --host <host>', 'адреса сервера')
+  .requiredOption('-p, --port <port>', 'порт сервера')
+  .requiredOption('-c, --cache <path>', 'шлях до кеш директорії');
 
-program.parse();
-const options = program.opts();
+program.parse(process.argv);
+const { host, port, cache } = program.opts();
 
-if (!options.host || !options.port || !options.cache) {
-  console.log('Потрібно вказати потрібні параметри -h, -p, -c');
-  process.exit(1);
-}
+if (!fs.existsSync(cache)) fs.mkdirSync(cache, { recursive: true });
 
-if (!fs.existsSync(options.cache)) {
-  fs.mkdirSync(options.cache, { recursive: true });
-}
+const upload = multer({ dest: cache });
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.end('Сервер запущено');
+let devices = [];
+let Id = 1;
+
+app.post('/register', upload.single('photo'), (req, res) => {
+  const { inventory_name, description } = req.body;
+  const photo = req.file;
+
+  if (!inventory_name || inventory_name.trim() === '') {
+    return res.status(400).end('Поле "inventory_name" є обов’язковим');
+  }
+
+  const device = {
+    id: Id++,
+    inventory_name,
+    description: description || '',
+    photo: photo ? photo.path : null
+  };
+
+  devices.push(device);
+
+  res.status(201).json({
+    message: 'Пристрій успішно зареєстровано',
+    device
+  });
 });
 
-server.listen(options.port, options.host, () => {
-  console.log(`Сервер запущено на адресі: http://${options.host}:${options.port}`);
+app.get('/inventory', (req, res) => {
+  res.json(devices);
 });
+
+
+const server = http.createServer(app);
+server.listen(port, host, () => { console.log(`Сервер запущено: http://${host}:${port}`);});
